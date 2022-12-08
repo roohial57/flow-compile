28e6f083c9884b2b831026e5ce4f46d96203622c
Roohallah Alizadeh <alizadeh.roohallah@gmail.com>
2022-12-08 17:17:10 +0330

    vg editor

---
+++ src/components/ChartEditor.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="flow-nav">
+    <v-g-editor  ref="editor1" height="700" width="800"> aaaaaaaaaaa
+      <flow :data="data" height="700" width="800" ref="flow1" />
+    </v-g-editor>
+  </div>
+</template>
+
+<script>
+import VGEditor, { Flow } from 'vg-editor'
+const data = {
+  nodes: [{
+    type: 'node',
+    size: '70*70',
+    shape: 'flow-circle',
+    color: '#FA8C16',
+    label: 'Start',
+    x: 55,
+    y: 55,
+    id: 'ea1184e8',
+    index: 0,
+  }, {
+    type: 'node',
+    size: '70*70',
+    shape: 'flow-circle',
+    color: '#FA8C16',
+    label: 'End',
+    x: 55,
+    y: 255,
+    id: '481fbb1a',
+    index: 2,
+  }],
+  edges: [{
+    source: 'ea1184e8',
+    sourceAnchor: 2,
+    target: '481fbb1a',
+    targetAnchor: 0,
+    id: '7989ac70',
+    index: 1,
+  }],
+};
+export default {
+  name: 'CharEditor',
+  data() {
+    return { data }
+  },
+  components: { VGEditor, Flow },
+  methods:{},
+  mounted(){
+    setTimeout(() => {
+      document.getElementById('canvas_1').height=700;
+      document.getElementById('canvas_1').width=800;
+    }, 3000);
+  }
+}
+</script> 
+<style>
+.flow-nav {
+  /* width: 800px;
+  height: 700px */
+}
+canvas#canvas_1 {
+    height: 700px !important;
+    width: 800px !important;
+    border: 1px solid;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <chart-editor/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ChartEditor from '@/components/ChartEditor.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    ChartEditor
   }
 }
 </script>
